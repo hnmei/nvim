38e09dd0e01ff784650ce580ec0148dda674c3f0
Haonan <meihaonan1015@gmail.com>
2024-11-29 21:41:03 +0800

    ADD Lualine Plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -2,17 +2,5 @@ return{
 	{
 	  -- NERDTree
 	  'scrooloose/nerdtree',
-	  
-	  -- Airline
-	  {
-		'vim-airline/vim-airline',
-		dependencies = {'vim-airline/vim-airline-themes'},
-		config = function()
-		  -- Airline Settings
-		  vim.g.airline_theme = 'everforest'
-		  vim.g.airline_powerline_fonts = 1
-		  vim.g['airline#extensions#tabline#enabled'] = 1
-		end
-	  },
 	}
 }
