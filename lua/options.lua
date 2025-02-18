77a521d5b2743ed2f1f55feb4d83b849cb1a8bac
Haonan <meihaonan1015@gmail.com>
2025-02-18 19:03:01 +0800

    SET different PDF viewer for different OS

--- lua/options.lua
+++ lua/options.lua
@@ -11,6 +11,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.opt.listchars = { tab = '>·' }
 vim.opt.list = true
 
