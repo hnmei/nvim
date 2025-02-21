1a18dece8355257ef0310519ff64137e9f6842e3
Haonan <meihaonan1015@gmail.com>
2025-02-21 16:17:27 +0800

    TEST visual placeholder

--- lua/luasnip/tex/math.lua
+++ lua/luasnip/tex/math.lua
@@ -1,3 +1,12 @@
+-- Enable UltiSnip-like visual placeholder using Lua 
+local get_visual = function(args, parent)
+  if (#parent.snippet.env.LS_SELECT_RAW > 0) then
+    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+  else  -- If LS_SELECT_RAW is empty, return a blank insert node
+    return sn(nil, i(1))
+  end
+end
+
 return{
 	s({ trig = ";a",},
 		{t("\\alpha"),}
@@ -6,8 +15,8 @@ return{
 	-- 	fmta(
 	-- 	"<>$<>$",
 	-- 	{
-	-- 		f( function(_, snip) return snip.captures[1] end ),
-	-- 		d(1, get_visual),
+	-- 	  f( function(_, snip) return snip.captures[1] end ),
+	-- 	  d(1, get_visual),
 	-- 	}
 	-- 	)
 	-- ),
