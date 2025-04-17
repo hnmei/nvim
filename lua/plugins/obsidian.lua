cc662a132fbd5422ced4a187e7786202c2db757a
Haonan <meihaonan1015@gmail.com>
2025-04-17 15:42:21 +0800

    UPDATE Obsidian Vault Path

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,6 +1,6 @@
 local sysname = vim.loop.os_uname().sysname
 if sysname == "Darwin" then
-	vault_path = "/Users/hnmei/OneDrive/TwenTwo"
+	vault_path = "/Users/hnmei/TwenTwo"
 elseif sysname == "Linux" then
 	vault_path = "/mnt/c/Users/hnmei/OneDrive/TwenTwo"
 end
