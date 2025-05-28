9d0fbc422f005441f19746874af837f82cf66fb9
Haonan <meihaonan1015@gmail.com>
2025-05-28 16:05:41 +0800

    UPDATE Commands Shortcuts for Window Navigation

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,4 +1,4 @@
--- Plugin Management (Using lazy.nvim)
+-- lugin Management (Using lazy.nvim)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
@@ -12,21 +12,35 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Key Mappings
+-- ## Key Mappings
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-	-- Modify default shortcuts
+-- ### Modify default shortcuts
 vim.keymap.set('n', 'n', 'nzz', { noremap = true, silent = true, desc = "Search next and center" })
 vim.keymap.set('n', 'N', 'Nzz', { noremap = true, silent = true, desc = "Search previous and center" })
 
-	-- Aliases for common commands
+-- ### Aliases for common commands
 vim.keymap.set("n", "<leader>s", "<Cmd>write<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>q", "<Cmd>quit<CR>", { noremap = true, silent = true })
 
-	-- Plugins shortcuts
+-- ### Navigation shortcuts
+vim.keymap.set('n', '<leader>ww', '<C-w>w', { desc = 'Cycle between open windows' })
+vim.keymap.set('n', '<leader>wv', '<C-w>v', { desc = 'Split window vertically' })
+vim.keymap.set('n', '<leader>ws', '<C-w>s', { desc = 'Split window horizontally' })
+vim.keymap.set('n', '<leader>wh', '<C-w>h', { desc = 'Go to left window' })
+vim.keymap.set('n', '<leader>wl', '<C-w>l', { desc = 'Go to right window' })
+vim.keymap.set('n', '<leader>wj', '<C-w>j', { desc = 'Go to below window' })
+vim.keymap.set('n', '<leader>wk', '<C-w>k', { desc = 'Go to above window' })
+vim.keymap.set('n', '<leader>wq', '<C-w>q', { desc = 'Close current window' })
+vim.keymap.set('n', '<leader>wo', '<C-w>o', { desc = 'Only keep current window' })
+vim.keymap.set('n', '<leader>w=', '<C-w>=', { desc = 'Make window sizes equal' })
+vim.keymap.set('n', '<leader>w_', '<C-w>_', { desc = 'Maximize current window height' })
+vim.keymap.set('n', '<leader>w|', '<C-w>|', { desc = 'Maximize current window width' })
+
+-- ### Plugins shortcuts
 vim.keymap.set("n", "<leader>v", "<plug>(vimtex-view)", { noremap = true, silent = true })
 
--- Plugin Configuration
+-- ### Plugin Configuration
 require("lazy").setup("plugins")
 
