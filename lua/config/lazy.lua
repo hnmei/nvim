2151eabd0453ca0003193c2dc9695a9a84e63d93
Haonan <meihaonan1015@gmail.com>
2025-05-28 15:52:50 +0800

    UPDATE Commands Shortcuts

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,16 @@ vim.opt.rtp:prepend(lazypath)
 -- Key Mappings
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+	-- Modify default shortcuts
+vim.keymap.set('n', 'n', 'nzz', { noremap = true, silent = true, desc = "Search next and center" })
+vim.keymap.set('n', 'N', 'Nzz', { noremap = true, silent = true, desc = "Search previous and center" })
+
+	-- Aliases for common commands
 vim.keymap.set("n", "<leader>s", "<Cmd>write<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>q", "<Cmd>quit<CR>", { noremap = true, silent = true })
+
+	-- Plugins shortcuts
 vim.keymap.set("n", "<leader>v", "<plug>(vimtex-view)", { noremap = true, silent = true })
 
 -- Plugin Configuration
